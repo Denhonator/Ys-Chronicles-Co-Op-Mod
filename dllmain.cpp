368e8b01e6f5f5d4b897e2c079044d19651e36ba
denhonator <elmeri.uotila@gmail.com>
2026-02-10 19:50:51 +0200

    Fixed tarf check, reset position camera based

--- dllmain.cpp
+++ dllmain.cpp
@@ -124,6 +124,8 @@ DWORD WINAPI HookThread(LPVOID) {
 	int TarfRoomCheckCode = baseAddress + 0x8DA04;
     int CanMove = baseAddress + 0x135884;
     int AdolRoom = baseAddress + 0x25c730;
+	int cam = baseAddress + 0x25D6D0;
+	int BlackOrbStatus = baseAddress + 0x25C8A6;
 
 	WriteBytes(ResetSpeedCode, "\x90\x90\x90\x90\x90\x90\x90", 7); //Nop out speed reset
 	WriteBytes(ResetAnimCode, "\x90\x90\x90\x90\x90\x90\x90", 7);
@@ -145,21 +147,21 @@ DWORD WINAPI HookThread(LPVOID) {
         }
 		int tarf = ReadValue<int>(TarfData);
 
-        bool allowTarf = ReadValue<int>(TarfStats) >= 10 || ReadValue<int>(AdolRoom) != 98;
+        bool allowTarf = ReadValue<short>(BlackOrbStatus) == 0x0100 || ReadValue<int>(AdolRoom) != 98;
 		WriteValue(adol + 0x24, 0.0f); //Reset Adol's speed to prevent sliding
 		WriteValue<char>(TarfToggle, allowTarf ? 44 : 0); //Keep Tarf active
-        if (ReadValue<int>(AdolRoom) == 81 && ReadValue<float>(adol + 0x18) < 700.0f) {
-            WriteValue(TarfStats, std::max<int>(10, ReadValue<int>(TarfStats))); // Picked up Tarf, allow progress
-        }
 
         if (ReadValue<int>(tarf) > baseAddress && ReadValue<int>(TarfUI) > 0) {
             WriteValue(tarf, 0x50B448);
             //WriteValue(tarf + 0x1D8, 1);  //Character type
             WriteValue(tarf + 0x1DC, 2);    //Character sprite
+			float prevMaxHP = ReadValue<short>(TarfStats + 4);
 			WriteValue(TarfStats + 4, ReadValue<short>(adol + 0xA4)); //Copy Tarf's HP from Adol's HP
             WriteValue(TarfStats + 6, ReadValue<short>(adol + 0xB0)); //Copy Tarf's STR from Adol's STR
             WriteValue(TarfStats + 8, ReadValue<short>(adol + 0xB4)); //Copy Tarf's DEF from Adol's DEF
 			short curHP = ReadValue<short>(adol + 0xA0);
+            if (prevMaxHP < ReadValue<short>(TarfStats + 4))
+				WriteValue(tarf + 0xA0, ReadValue<short>(TarfStats + 4)); //If Tarf's max HP increased, heal him to full
             if(curHP > adolLastHP && adolLastHP > 0)
 				WriteValue(tarf + 0xA0, ReadValue<short>(tarf + 0xA0) + curHP - adolLastHP);
 
@@ -184,10 +186,12 @@ DWORD WINAPI HookThread(LPVOID) {
             else
                 WriteValue(tarf + 0x24, 0.0f);
 
-            if(std::abs(ReadValue<float>(tarf + 0x14) - ReadValue<float>(adol + 0x14)) > maxDistanceX
-            || std::abs(ReadValue<float>(tarf + 0x18) - ReadValue<float>(adol + 0x18)) > maxDistanceY) {
+			float camCenterX = ReadValue<int>(cam) + 296.0f;
+			float camCenterY = ReadValue<int>(cam+4) + 192.0f;
+            if(std::abs(ReadValue<float>(tarf + 0x14) - camCenterX) > maxDistanceX
+            || std::abs(ReadValue<float>(tarf + 0x18) - camCenterY) > maxDistanceY) {
                 WriteValue(tarf + 0x14, ReadValue<float>(adol + 0x14)+25.0f);
-                WriteValue(tarf + 0x18, ReadValue<float>(adol + 0x18)+25.0f);
+                WriteValue(tarf + 0x18, ReadValue<float>(adol + 0x18)-25.0f);
             }
         }
 
@@ -212,9 +216,37 @@ BOOL APIENTRY DllMain(HMODULE hModule, DWORD dwReason, LPVOID lpReserved) {
     return TRUE;
 }
 
-#pragma comment(linker, "/export:SymInitialize=dbghelp_original.SymInitialize")
 #pragma comment(linker, "/export:SymCleanup=dbghelp_original.SymCleanup")
 #pragma comment(linker, "/export:SymFromAddr=dbghelp_original.SymFromAddr")
+#pragma comment(linker, "/export:SymGetSymFromAddr=dbghelp_original.SymGetSymFromAddr")
+#pragma comment(linker, "/export:SymGetSymFromAddr64=dbghelp_original.SymGetSymFromAddr64")
 #pragma comment(linker, "/export:SymGetOptions=dbghelp_original.SymGetOptions")
+#pragma comment(linker, "/export:SymLoadModule64=dbghelp_original.SymLoadModule64")
+#pragma comment(linker, "/export:SymLoadModule=dbghelp_original.SymLoadModule")
+#pragma comment(linker, "/export:SymGetModuleInfo64=dbghelp_original.SymGetModuleInfo64")
+#pragma comment(linker, "/export:SymGetModuleBase64=dbghelp_original.SymGetModuleBase64")
+#pragma comment(linker, "/export:SymGetLineFromAddr64=dbghelp_original.SymGetLineFromAddr64")
 #pragma comment(linker, "/export:SymSetOptions=dbghelp_original.SymSetOptions")
 #pragma comment(linker, "/export:MiniDumpWriteDump=dbghelp_original.MiniDumpWriteDump")
+#pragma comment(linker, "/export:StackWalk64=dbghelp_original.StackWalk64")
+#pragma comment(linker, "/export:UnDecorateSymbolName=dbghelp_original.UnDecorateSymbolName")
+#pragma comment(linker, "/export:SymInitialize=dbghelp_original.SymInitialize")
+#pragma comment(linker, "/export:SymFunctionTableAccess64=dbghelp_original.SymFunctionTableAccess64")
+#pragma comment(linker, "/export:SymGetModuleInfo=dbghelp_original.SymGetModuleInfo")
+#pragma comment(linker, "/export:SymGetSearchPath=dbghelp_original.SymGetSearchPath")
+#pragma comment(linker, "/export:SymSetSearchPath=dbghelp_original.SymSetSearchPath")
+#pragma comment(linker, "/export:SymEnumSymbols=dbghelp_original.SymEnumSymbols")
+#pragma comment(linker, "/export:SymFromName=dbghelp_original.SymFromName")
+#pragma comment(linker, "/export:SymGetTypeInfo=dbghelp_original.SymGetTypeInfo")
+#pragma comment(linker, "/export:SymRegisterCallback64=dbghelp_original.SymRegisterCallback64")
+#pragma comment(linker, "/export:SymLoadModuleEx=dbghelp_original.SymLoadModuleEx")
+#pragma comment(linker, "/export:SymUnloadModule64=dbghelp_original.SymUnloadModule64")
+#pragma comment(linker, "/export:SymGetLineFromName64=dbghelp_original.SymGetLineFromName64")
+#pragma comment(linker, "/export:SymEnumerateModules64=dbghelp_original.SymEnumerateModules64")
+#pragma comment(linker, "/export:SymGetModuleBase=dbghelp_original.SymGetModuleBase")
+#pragma comment(linker, "/export:ImageDirectoryEntryToData=dbghelp_original.ImageDirectoryEntryToData")
+#pragma comment(linker, "/export:ImageNtHeader=dbghelp_original.ImageNtHeader")
+#pragma comment(linker, "/export:ImageRvaToVa=dbghelp_original.ImageRvaToVa")
+#pragma comment(linker, "/export:SymFindFileInPath=dbghelp_original.SymFindFileInPath")
+#pragma comment(linker, "/export:SymMatchString=dbghelp_original.SymMatchString")
+#pragma comment(linker, "/export:SymSearch=dbghelp_original.SymSearch")
