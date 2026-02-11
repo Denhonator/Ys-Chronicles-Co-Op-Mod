5774e49d0744a381255a1cc02bb5853f7c7c2277
denhonator <elmeri.uotila@gmail.com>
2026-02-11 05:55:25 +0200

    Ys1 mod progress

--- dllmain.cpp
+++ dllmain.cpp
@@ -10,6 +10,9 @@
 
 HMODULE hOriginal = nullptr;
 int baseAddress = 0;
+char UP = 'I', DOWN = 'K', LEFT = 'J', RIGHT = 'L', WALK = 'U';
+float maxDistanceX = 400.0f;
+float maxDistanceY = 250.0f;
 
 int FindPointer(int offsets[], int count)
 {
@@ -52,6 +55,185 @@ void WriteBytes(int offset, const char* bytes, size_t size) {
     VirtualProtect((void*)offset, size, oldProtect, &oldProtect);
 }
 
+void Ys2() {
+    int posXAddr[2] = { baseAddress + 0x1453D0, 0x14 };
+    int TarfToggle = baseAddress + 0x25d3f0;
+    int AdolData = baseAddress + 0x1453D0;
+    int TarfData = baseAddress + 0x1453D8;
+    int TarfScan[2] = { baseAddress + 0x1453D4, 0x1DC };
+    int TarfUI = baseAddress + 0x25D3F4;
+    int TarfStats = baseAddress + 0x12C3F0;
+    int ResetSpeedCode = baseAddress + 0x1A0D4;
+    int ResetAnimCode = baseAddress + 0x1A0E3;
+    int TarfRoomCheckCode = baseAddress + 0x8DA04;
+    int CanMove = baseAddress + 0x135884;
+    int AdolRoom = baseAddress + 0x25c730;
+    int cam = baseAddress + 0x25D6D0;
+    int BlackOrbStatus = baseAddress + 0x25C8A6;
+
+    WriteBytes(ResetSpeedCode, "\x90\x90\x90\x90\x90\x90\x90", 7); //Nop out speed reset
+    WriteBytes(ResetAnimCode, "\x90\x90\x90\x90\x90\x90\x90", 7);
+    WriteBytes(TarfRoomCheckCode, "\x90\x90\xB3\x00", 4);
+
+    int adolLastHP = ReadValue<short>(AdolData + 0xA0);
+
+    while (true) {
+        Sleep(10);
+
+        int adol = ReadValue<int>(AdolData);
+        TarfScan[0] = AdolData;
+        while (ReadValue<int>(TarfScan[0]) >= baseAddress) {
+            if (ReadValue<int>(TarfScan, 2) == 0x2C) {
+                TarfData = TarfScan[0];
+                break;
+            }
+            TarfScan[0] += 4;
+        }
+        int tarf = ReadValue<int>(TarfData);
+
+        bool allowTarf = ReadValue<short>(BlackOrbStatus) == 0x0100 || ReadValue<int>(AdolRoom) != 98;
+        WriteValue(adol + 0x24, 0.0f); //Reset Adol's speed to prevent sliding
+        WriteValue<char>(TarfToggle, allowTarf ? 44 : 0); //Keep Tarf active
+
+        if (ReadValue<int>(tarf) > baseAddress && ReadValue<int>(TarfUI) > 0) {
+            WriteValue(tarf, 0x50B448);
+            //WriteValue(tarf + 0x1D8, 1);  //Character type
+            WriteValue(tarf + 0x1DC, 2);    //Character sprite
+            float prevMaxHP = ReadValue<short>(TarfStats + 4);
+            WriteValue(TarfStats + 4, ReadValue<short>(adol + 0xA4)); //Copy Tarf's HP from Adol's HP
+            WriteValue(TarfStats + 6, ReadValue<short>(adol + 0xB0)); //Copy Tarf's STR from Adol's STR
+            WriteValue(TarfStats + 8, ReadValue<short>(adol + 0xB4)); //Copy Tarf's DEF from Adol's DEF
+            short curHP = ReadValue<short>(adol + 0xA0);
+            if (prevMaxHP < ReadValue<short>(TarfStats + 4))
+                WriteValue(tarf + 0xA0, ReadValue<short>(TarfStats + 4)); //If Tarf's max HP increased, heal him to full
+            if (curHP > adolLastHP && adolLastHP > 0)
+                WriteValue(tarf + 0xA0, ReadValue<short>(tarf + 0xA0) + curHP - adolLastHP);
+
+            float verticalDir = (GetKeyState(UP) & 0x8000) ? 1.0f : ((GetKeyState(DOWN) & 0x8000) ? -1.0f : 0.0f);
+            float horizontalDir = (GetKeyState(RIGHT) & 0x8000) ? 1.0f : ((GetKeyState(LEFT) & 0x8000) ? -1.0f : 0.0f);
+            float speed = GetKeyState(WALK) & 0x8000 ? 1.5f : 3.5f;
+
+            if ((verticalDir != 0 || horizontalDir != 0) && !ReadValue<int>(CanMove)) {
+                float rotation = 0;
+                if (verticalDir < 0)
+                    rotation = 1024.0f - horizontalDir * 512.0f;
+                else if (verticalDir > 0)
+                    rotation = 3072.0f + horizontalDir * 512.0f;
+                else
+                    rotation = horizontalDir < 0 ? 2048.0f : 0.0f;
+
+                WriteValue(tarf + 0x24, speed);
+                WriteValue(tarf + 0x28, rotation);
+                WriteValue(tarf + 0x2C, rotation);
+                WriteValue<char>(tarf + 0x1D1, speed > 2.0f ? 1 : 0);
+            }
+            else
+                WriteValue(tarf + 0x24, 0.0f);
+
+            float camCenterX = ReadValue<int>(cam) + 296.0f;
+            float camCenterY = ReadValue<int>(cam + 4) + 192.0f;
+            if (std::abs(ReadValue<float>(tarf + 0x14) - camCenterX) > maxDistanceX
+                || std::abs(ReadValue<float>(tarf + 0x18) - camCenterY) > maxDistanceY) {
+                WriteValue(tarf + 0x14, ReadValue<float>(adol + 0x14) + 25.0f);
+                WriteValue(tarf + 0x18, ReadValue<float>(adol + 0x18) - 25.0f);
+            }
+        }
+
+        adolLastHP = ReadValue<short>(adol + 0xA0);
+
+        //float posX = ReadValue<float>(posXAddr, 2);
+        //sprintf_s(buffer, "PosX: %.f", posX);
+        //OutputDebugStringA(buffer);
+    }
+}
+
+void Ys1() {
+    int FeenaActive = baseAddress + 0x131C44;
+    int AdolData = baseAddress + 0x14061C;
+    int FeenaData = baseAddress + 0x142C2C;
+    int AdolCurStats = baseAddress + 0x131694;
+    int AdolHP = baseAddress + 0x1317FC;
+    int FeenaHP = AdolHP + 0x20;
+    int ResetSpeedCode = baseAddress + 0x1B9E0;
+    int MoveAnimCode = baseAddress + 0x17F26;
+    int FeenaRoomCheckCode = baseAddress + 0x351A2;
+    int RunToggleCode = baseAddress + 0x1BE1A;
+    //int RunAudioCode = baseAddress + 0x1BA86;
+    int CanMove = baseAddress + 0x135884;
+    int AdolRoom = baseAddress + 0x11E494;
+    int FeenaRoom = baseAddress + 0x13169C;
+    int NextRoom = baseAddress + 0x131554;
+    int cam = baseAddress + 0x127DFC;
+
+	WriteBytes(ResetSpeedCode, "\xEB\x0F", 2); //jmp over speed reset
+    WriteBytes(RunToggleCode, "\x66\x81\x7B\x18\x80\x02\x76\x04", 8);
+    //WriteBytes(RunAudioCode, "\x66\x81\x7B\x18\x80\x02\x90", 7);
+    //WriteBytes(MoveAnimCode, "\x83\x7B\x18\x00\x90", 5);
+    WriteBytes(FeenaRoomCheckCode, "\x90\x90", 2);
+
+    int adolLastHP = ReadValue<short>(AdolHP);
+
+    while (true) {
+        Sleep(10);
+
+        int adol = ReadValue<int>(AdolData);
+        int feena = ReadValue<int>(FeenaData);
+
+        bool allowFeena = true;
+        //WriteValue(adol + 0x24, 0.0f); //Reset Adol's speed to prevent sliding
+        WriteValue<char>(FeenaActive, allowFeena ? 1 : 0); //Keep Feena active
+
+        if (ReadValue<int>(feena) == 0x4DB12C || ReadValue<int>(feena) == 0x4DAE1C) {
+            int nextRoom = ReadValue<int>(NextRoom);
+            if (nextRoom > 0)
+                WriteValue(FeenaRoom, ReadValue<int>(NextRoom)); //Keep Feena in the same room as Adol
+            WriteValue(feena, 0x4DAE1C);
+            WriteValue<char>(feena + 0x250, 1); //Allows running without slowing down
+            //WriteValue(tarf + 0x1D8, 1);  //Character type
+            WriteValue(feena + 0x4C, ReadValue<int>(adol+0x4C));    //Character sprite
+            float prevMaxHP = ReadValue<short>(FeenaHP + 4);
+            WriteValue(FeenaHP + 4, ReadValue<short>(AdolHP+4));        //Copy Feena's HP from Adol's HP
+            WriteValue(FeenaHP + 8, ReadValue<short>(AdolCurStats));    //Copy Feena's STR from Adol's STR
+            WriteValue(FeenaHP + 12, ReadValue<short>(AdolCurStats+4)); //Copy Feena's DEF from Adol's DEF
+            short curHP = ReadValue<short>(AdolHP);
+            if (prevMaxHP < ReadValue<short>(FeenaHP + 4))
+                WriteValue(FeenaHP, ReadValue<short>(FeenaHP + 4)); //If Feena's max HP increased, heal him to full
+            if (curHP > adolLastHP && adolLastHP > 0)
+                WriteValue(FeenaHP, ReadValue<short>(FeenaHP) + curHP - adolLastHP);
+
+            int verticalDir = (GetKeyState(UP) & 0x8000) ? 1 : ((GetKeyState(DOWN) & 0x8000) ? -1 : 0);
+            int horizontalDir = (GetKeyState(RIGHT) & 0x8000) ? 1 : ((GetKeyState(LEFT) & 0x8000) ? -1 : 0);
+            int speed = GetKeyState(WALK) & 0x8000 ? 640 : 1152;
+
+            if ((verticalDir != 0 || horizontalDir != 0) && !ReadValue<int>(CanMove)) {
+                int rotation = 0;
+                if (verticalDir > 0)
+                    rotation = 64 - horizontalDir * 32;
+                else if (verticalDir < 0)
+                    rotation = 192 + horizontalDir * 32;
+                else
+                    rotation = horizontalDir < 0 ? 128 : 0;
+
+                WriteValue(feena + 0x18, speed);
+                WriteValue(feena + 0x14, rotation);
+                //WriteValue<char>(tarf + 0x1D1, speed > 2.0f ? 1 : 0);
+            }
+            else
+                WriteValue(feena + 0x18, 0);
+
+            int camCenterX = ReadValue<int>(cam) + 296;
+            int camCenterY = ReadValue<int>(cam + 4) + 192;
+            if (std::abs(ReadValue<int>(feena + 0x24) - camCenterX) > maxDistanceX
+                || std::abs(ReadValue<int>(feena + 0x28) - camCenterY) > maxDistanceY) {
+                WriteValue(feena + 0x24, ReadValue<int>(adol + 0x24) + 25);
+                WriteValue(feena + 0x28, ReadValue<int>(adol + 0x28) - 25);
+            }
+        }
+
+        adolLastHP = ReadValue<short>(AdolHP);
+    }
+}
+
 DWORD WINAPI HookThread(LPVOID) {
 	OutputDebugStringA("HookThread started");
 
@@ -73,14 +255,12 @@ DWORD WINAPI HookThread(LPVOID) {
     sprintf_s(buffer, "Process name: %s\n", filename);
     OutputDebugStringA(buffer);
 
-    if(std::strcmp(filename, "ys2plus.exe")) {
-        OutputDebugStringA("Not ys2plus.exe, exiting thread");
+	int game = std::strcmp(filename, "ys1plus.exe") == 0 ? 1 : std::strcmp(filename, "ys2plus.exe") == 0 ? 2 : 0;
+    if(game == 0) {
+        OutputDebugStringA("Not ys1plus.exe or ys2plus.exe, exiting thread");
         return 0;
 	}
 
-	char UP = 'I', DOWN = 'K', LEFT = 'J', RIGHT = 'L', WALK = 'U';
-	float maxDistanceX = 400.0f;
-	float maxDistanceY = 250.0f;
 	std::ifstream config("coop_config.txt");
 	// If the config file doesn't exist, create it with default values
 	if (!config.is_open()) {
@@ -112,95 +292,11 @@ DWORD WINAPI HookThread(LPVOID) {
         }
     }
 
-    int posXAddr[2] = { baseAddress + 0x1453D0, 0x14 };
-	int TarfToggle = baseAddress + 0x25d3f0;
-	int AdolData = baseAddress + 0x1453D0;
-	int TarfData = baseAddress + 0x1453D8;
-	int TarfScan[2] = { baseAddress + 0x1453D4, 0x1DC };
-	int TarfUI = baseAddress + 0x25D3F4;
-    int TarfStats = baseAddress + 0x12C3F0;
-	int ResetSpeedCode = baseAddress + 0x1A0D4;
-	int ResetAnimCode = baseAddress + 0x1A0E3;
-	int TarfRoomCheckCode = baseAddress + 0x8DA04;
-    int CanMove = baseAddress + 0x135884;
-    int AdolRoom = baseAddress + 0x25c730;
-	int cam = baseAddress + 0x25D6D0;
-	int BlackOrbStatus = baseAddress + 0x25C8A6;
-
-	WriteBytes(ResetSpeedCode, "\x90\x90\x90\x90\x90\x90\x90", 7); //Nop out speed reset
-	WriteBytes(ResetAnimCode, "\x90\x90\x90\x90\x90\x90\x90", 7);
-	WriteBytes(TarfRoomCheckCode, "\x90\x90\xB3\x00", 4);
-
-	int adolLastHP = ReadValue<short>(AdolData + 0xA0);
-
-    while (true) {
-        Sleep(10);
-
-		int adol = ReadValue<int>(AdolData);
-        TarfScan[0] = AdolData;
-        while (ReadValue<int>(TarfScan[0]) >= baseAddress) {
-            if (ReadValue<int>(TarfScan, 2) == 0x2C) {
-				TarfData = TarfScan[0];
-                break;
-            }
-			TarfScan[0] += 4;
-        }
-		int tarf = ReadValue<int>(TarfData);
-
-        bool allowTarf = ReadValue<short>(BlackOrbStatus) == 0x0100 || ReadValue<int>(AdolRoom) != 98;
-		WriteValue(adol + 0x24, 0.0f); //Reset Adol's speed to prevent sliding
-		WriteValue<char>(TarfToggle, allowTarf ? 44 : 0); //Keep Tarf active
-
-        if (ReadValue<int>(tarf) > baseAddress && ReadValue<int>(TarfUI) > 0) {
-            WriteValue(tarf, 0x50B448);
-            //WriteValue(tarf + 0x1D8, 1);  //Character type
-            WriteValue(tarf + 0x1DC, 2);    //Character sprite
-			float prevMaxHP = ReadValue<short>(TarfStats + 4);
-			WriteValue(TarfStats + 4, ReadValue<short>(adol + 0xA4)); //Copy Tarf's HP from Adol's HP
-            WriteValue(TarfStats + 6, ReadValue<short>(adol + 0xB0)); //Copy Tarf's STR from Adol's STR
-            WriteValue(TarfStats + 8, ReadValue<short>(adol + 0xB4)); //Copy Tarf's DEF from Adol's DEF
-			short curHP = ReadValue<short>(adol + 0xA0);
-            if (prevMaxHP < ReadValue<short>(TarfStats + 4))
-				WriteValue(tarf + 0xA0, ReadValue<short>(TarfStats + 4)); //If Tarf's max HP increased, heal him to full
-            if(curHP > adolLastHP && adolLastHP > 0)
-				WriteValue(tarf + 0xA0, ReadValue<short>(tarf + 0xA0) + curHP - adolLastHP);
-
-			float verticalDir = (GetKeyState(UP) & 0x8000) ? 1.0f : ((GetKeyState(DOWN) & 0x8000) ? -1.0f : 0.0f);
-			float horizontalDir = (GetKeyState(RIGHT) & 0x8000) ? 1.0f : ((GetKeyState(LEFT) & 0x8000) ? -1.0f : 0.0f);
-            float speed = GetKeyState(WALK) & 0x8000 ? 1.5f : 3.5f;
-
-            if((verticalDir != 0 || horizontalDir != 0) && !ReadValue<int>(CanMove)) {
-                float rotation = 0;
-                if (verticalDir < 0)
-                    rotation = 1024.0f - horizontalDir * 512.0f;
-                else if (verticalDir > 0)
-                    rotation = 3072.0f + horizontalDir * 512.0f;
-                else
-					rotation = horizontalDir < 0 ? 2048.0f : 0.0f;
-
-                WriteValue(tarf + 0x24, speed);
-				WriteValue(tarf + 0x28, rotation);
-				WriteValue(tarf + 0x2C, rotation);
-				WriteValue<char>(tarf + 0x1D1, speed > 2.0f ? 1 : 0);
-			}
-            else
-                WriteValue(tarf + 0x24, 0.0f);
-
-			float camCenterX = ReadValue<int>(cam) + 296.0f;
-			float camCenterY = ReadValue<int>(cam+4) + 192.0f;
-            if(std::abs(ReadValue<float>(tarf + 0x14) - camCenterX) > maxDistanceX
-            || std::abs(ReadValue<float>(tarf + 0x18) - camCenterY) > maxDistanceY) {
-                WriteValue(tarf + 0x14, ReadValue<float>(adol + 0x14)+25.0f);
-                WriteValue(tarf + 0x18, ReadValue<float>(adol + 0x18)-25.0f);
-            }
-        }
-
-        adolLastHP = ReadValue<short>(adol + 0xA0);
-
-        //float posX = ReadValue<float>(posXAddr, 2);
-        //sprintf_s(buffer, "PosX: %.f", posX);
-        //OutputDebugStringA(buffer);
-    }
+    if (game == 1)
+		Ys1();
+    else if (game == 2)
+        Ys2();
+    
     return 0;
 }
 
